bf3026cc82bf8d61cc14f32496064638c413df0b
Priyanshi Palariya <priyapalariya26@gmail.com>
2022-09-23 07:37:42 +0530

    md to lg

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,7 @@ import Product from "./Product";
 
 function ProductList({ products }) {
   return (
-    <div className=" flex flex-wrap gap-2 rounded-md space-y-3 md:space-y-0 mx-auto">
+    <div className=" lg:grid lg:grid-cols-3 gap-2 rounded-md space-y-3 md:space-y-0 mx-auto">
       {products.map(function(item) {
         return (
           <Product
